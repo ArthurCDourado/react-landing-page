b58fd593ab73716dc406ab7922fea21fd2bc1a66
Arthur C <arthurdourado01@hotmail.com>
2024-05-28 15:08:00 -0300

    Benefits, Collaboration, Bento Box sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits'
+import Colaboration from './components/Colaboration'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
         <Hero />
+        <Benefits />
+        <Colaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
